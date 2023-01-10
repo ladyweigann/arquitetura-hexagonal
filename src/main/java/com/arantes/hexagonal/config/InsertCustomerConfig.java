6228e90ea50ec4da49ddd8b19920ed18a195b1d7
Larissa Labanca <ladyweigann@gmail.com>
2023-01-10 17:29:15 -0300

    Criação dos beans de configuração dos use cases

---
+++ src/main/java/com/arantes/hexagonal/config/InsertCustomerConfig.java
@@ -0,0 +1,16 @@
+package com.arantes.hexagonal.config;
+
+import com.arantes.hexagonal.adapters.out.FindAddressByZipCodeAdapter;
+import com.arantes.hexagonal.adapters.out.InsertCustomerAdapter;
+import com.arantes.hexagonal.application.core.usecase.InsertCustomerUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InsertCustomerConfig {
+
+    @Bean
+    public InsertCustomerUseCase insertCustomerUseCase(FindAddressByZipCodeAdapter findAddressByZipCodeAdapter, InsertCustomerAdapter insertCustomerAdapter) {
+        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter);
+    }
+}
